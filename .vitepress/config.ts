e449c11506a681b4f7850b605a6628c12c6dacc6
zoumingyin <37207572+zoumingyin@users.noreply.github.com>
2025-08-19 14:30:09 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "我的文档站",
   description: "功能齐全的 VitePress 文档",
-  base: "/place-keep-doc/",
+  base: "/",
   srcDir: "./docs",
   themeConfig: {
     logo: "/logo.svg",
