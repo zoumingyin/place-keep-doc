8f967bb95e4c977502ef8876af8617364cde2fdd
Terry <314292262@qq.com>
2025-08-19 16:50:48 +0800

    chore: 更新部署及文档配置

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -30,5 +30,21 @@ export default defineConfig({
       { icon: "github", link: "https://github.com/zoumingyin/place-keep-doc" },
     ],
     darkModeSwitchLabel: "Appearance",
+    // --- 页面底部的 "在 GitHub 上编辑此页" ---
+    editLink: {
+      pattern:
+        "https://github.com/zoumingyin/place-keep-doc/edit/main/docs/:path", // 请替换
+      text: "在 GitHub 上编辑此页面",
+    },
+
+    // --- 最后更新时间 ---
+    lastUpdated: { text: "Last updated" }, // 启用
+    lastUpdatedText: "最后更新于", // 显示的文本
+
+    // --- 页脚 ---
+    footer: {
+      message: "基于 MIT 许可发布",
+      copyright: "Copyright © 2024-present zoumingyin",
+    },
   },
 });
